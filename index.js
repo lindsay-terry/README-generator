d34744aa33fd91706fc076e32d30fe9c6ad0c923
Lindsay Terry <lindsaytee66@gmail.com>
2024-06-11 10:36:26 -0600

    Wrote functions to successfully write new file with user input

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
@@ -63,12 +64,21 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    fs.writeFile('generatedREADME.md', JSON.stringify(data), (err) => {
+        if (err)
+            console.log("There's been an error.  Please try again.");
+        else {
+            console.log('README has been successfully generated.  Please see generatedREADME.md for your README!');
+        }
+    })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions);
+    inquirer.prompt(questions)
+    .then((answers) => {
+        writeToFile('generatedREADME.md', answers);
+    });
 }
 
 // Function call to initialize app
