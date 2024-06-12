ad1d561685e3328c39a7abc74c4cf7c4c862f9ce
Lindsay Terry <lindsaytee66@gmail.com>
2024-06-11 20:33:11 -0600

    Added validation to each question in index.js

--- index.js
+++ index.js
@@ -9,18 +9,37 @@ const questions = [
         type: 'input',
         name: 'username',
         message: 'Please enter your Github username.',
+        validate: function(input) {
+            if (!input) {
+                return 'Please enter a valid Github username'
+            } 
+            return true;
+        }
     },
 
     {
         type: 'input',
         name: 'email',
         message: 'Please enter your email address.',
+        validate: function(input) {
+            const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+            if (!regex.test(input)) {
+                return 'Please enter a valid email address'
+            }
+            return true;
+        }
     },
 
     {
         type: 'input',
         name: 'projectName',
         message: 'What is the name of your project?',
+        validate: function(input) {
+            if (!input) {
+                return 'Please enter the name of your project.'
+            }
+            return true;
+        }
 
     },
 
@@ -28,6 +47,12 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'Please write a description of your project.',
+        validate: function(input) {
+            if (!input) {
+                return 'Please enter a description of your project.'
+            }
+            return true;
+        }
     },
 
     {
@@ -35,30 +60,60 @@ const questions = [
         name: 'license',
         message: 'What kind of license does your project need?',
         choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        validate: function(input) {
+            if (!['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'].includes(input)) {
+                return 'Please select a valid license option'
+            }
+            return true;
+        }
     },
 
     {
         type: 'input',
         name: 'dependencies',
-        message: 'Is there a command needed to install dependencies?',
+        message: 'What command is needed to install dependencies?',
+        validate: function(input) {
+            if (!input) {
+                return 'Please enter the command to install dependencies or write N/A.'
+            }
+            return true;
+        }
     },
 
     {
         type: 'input',
         name: 'tests',
         message: 'What command is needed to run tests?',
+        validate: function(input) {
+            if (!input) {
+                return 'Please enter the command to run tests or write N/A'
+            }
+            return true;
+        }
     },
 
     {
         type: 'input',
         name: 'usage',
         message: 'What does the user need to know about the project?',
+        validate: function(input) {
+            if (!input) {
+                return 'Please enter usage information about your project.'
+            }
+            return true;
+        }
     },
 
     {
         type: 'input', 
         name: 'contributions',
         message: 'What does the user need to know about contributing to the project?',
+        validate: function(input) {
+            if (!input) {
+                return 'Please include information about contributing to your project or write N/A'
+            }
+            return true;
+        }
     },
 ];
 
